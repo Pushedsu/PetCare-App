46a6d6101558e005740529f890b9d5b8560cb71f
민수 <109027302+Pushedsu@users.noreply.github.com>
2023-11-11 16:47:13 +0900

    검색 기능 추가

--- lib/connect/connect_server.dart
+++ lib/connect/connect_server.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:pet_care/utils/app_constants.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:pet_care/env/env.dart';
 
 class Put {
   Future<http.Response> plusLike(String id) async {
     final response = await http.put(
-      Uri.parse('http://${appConstants.UriIp}:3000/posts/${id}'),
+      Uri.parse('http://${env.UriIp}:3000/posts/${id}'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
@@ -17,7 +18,7 @@ class Put {
 class Get {
   Future<http.Response> getAllPosts() async {
     final response = await http.get(
-      Uri.parse('http://${appConstants.UriIp}:3000/posts/all'),
+      Uri.parse('http://${env.UriIp}:3000/posts/all'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
@@ -26,31 +27,17 @@ class Get {
   }
   Future<http.Response> getMyPosts(String id) async{
     final response = await http.get(
-      Uri.parse('http://${appConstants.UriIp}:3000/posts/${id}'),
+      Uri.parse('http://${env.UriIp}:3000/posts/${id}'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
     );
     return response;
   }
-  // Future<AllUserData> getAllUser() async {
-  //   final response = await http.get(
-  //     Uri.parse('http://${appConstants.UriIp}:3000/user/all'),
-  //     headers: <String, String>{
-  //       'Content-Type': 'application/json',
-  //     },
-  //   );
-  //   if(response.statusCode == 200 ) {
-  //     return AllUserData.fromJson(jsonDecode(response.body));
-  //   } else {
-  //     print(response.body.toString());
-  //     throw Exception('Failed to load post');
-  //   }
-  // }
 
   Future<http.Response> getUserInfo(String accessToken) async {
     final response = await http.get(
-      Uri.parse('http://${appConstants.UriIp}:3000/user/userInfo'),
+      Uri.parse('http://${env.UriIp}:3000/user/userInfo'),
       headers: <String, String>{
       'Content-Type': 'application/json',
       'Authorization': 'Bearer $accessToken',
@@ -60,7 +47,7 @@ class Get {
   }
   Future<http.Response> getAccessToken(String refreshToken) async {
     final response = await http.get(
-      Uri.parse('http://${appConstants.UriIp}:3000/user/getAccessToken'),
+      Uri.parse('http://${env.UriIp}:3000/user/getAccessToken'),
       headers: <String, String>{
         'Content-Type': 'application/json',
         'Authorization': 'Bearer $refreshToken',
@@ -73,7 +60,7 @@ class Get {
 class Post {
   Future<http.Response> signUp(Map<String, dynamic> data) async{
     final response = await http.post(
-      Uri.parse('http://${appConstants.UriIp}:3000/user/signUp'),
+      Uri.parse('http://${env.UriIp}:3000/user/signUp'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
@@ -84,7 +71,7 @@ class Post {
 
   Future<http.Response> login(Map<String, dynamic> data) async{
     final response =  await http.post(
-      Uri.parse('http://${appConstants.UriIp}:3000/user/login'),
+      Uri.parse('http://${env.UriIp}:3000/user/login'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
@@ -94,7 +81,7 @@ class Post {
   }
   Future<http.Response> createPost(Map<String, dynamic> data) async{
     final response =  await http.post(
-      Uri.parse('http://${appConstants.UriIp}:3000/posts/posting'),
+      Uri.parse('http://${env.UriIp}:3000/posts/posting'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
@@ -104,7 +91,7 @@ class Post {
   }
   Future<http.Response> deleteUser(String accessToken, Map<String, dynamic> data) async{
     final response =  await http.post(
-      Uri.parse('http://${appConstants.UriIp}:3000/user/deleteUser'),
+      Uri.parse('http://${env.UriIp}:3000/user/deleteUser'),
       headers: <String, String>{
         'Content-Type': 'application/json',
         'Authorization': 'Bearer $accessToken',
@@ -115,7 +102,7 @@ class Post {
   }
   Future<http.Response> updateName(Map<String, dynamic> data) async{
     final response = await http.post(
-      Uri.parse('http://${appConstants.UriIp}:3000/user/updateName'),
+      Uri.parse('http://${env.UriIp}:3000/user/updateName'),
       headers: <String, String>{
       'Content-Type': 'application/json',
       },
@@ -125,7 +112,57 @@ class Post {
   }
   Future<http.Response> updatePassword(Map<String, dynamic> data) async{
     final response = await http.post(
-      Uri.parse('http://${appConstants.UriIp}:3000/user/updatePassword'),
+      Uri.parse('http://${env.UriIp}:3000/user/updatePassword'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(data),
+    );
+    return response;
+  }
+  Future<http.Response> searchTitle(Map<String, dynamic> data) async{
+    final response = await http.post(
+      Uri.parse('http://${env.UriIp}:3000/posts/searchTitle'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(data),
+    );
+    return response;
+  }
+  Future<http.Response> searchContents(Map<String, dynamic> data) async{
+    final response = await http.post(
+      Uri.parse('http://${env.UriIp}:3000/posts/searchContents'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(data),
+    );
+    return response;
+  }
+  Future<http.Response> updateImg(XFile imageFile, String id) async{
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('http://${env.UriIp}:3000/user/uploadImg'),
+    );
+
+    // 이미지 파일 추가
+    request.files.add(
+      await http.MultipartFile.fromPath(
+        'image',
+        imageFile.path,
+      ),
+    );
+
+    // id를 form-data로 추가
+    request.fields['id'] = id;
+
+    var response = await request.send();
+    return await http.Response.fromStream(response);
+  }
+  Future<http.Response> deleteImg(Map<String, dynamic> data) async{
+    final response = await http.post(
+      Uri.parse('http://${env.UriIp}:3000/user/deleteImg'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
@@ -138,7 +175,7 @@ class Post {
 class Delete {
   Future<http.Response> logout(String id) async{
     final response =  await http.delete(
-      Uri.parse('http://${appConstants.UriIp}:3000/user/$id'),
+      Uri.parse('http://${env.UriIp}:3000/user/$id'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
