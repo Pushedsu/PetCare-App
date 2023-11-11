62d6be9cbb8a3fc787d5d198ea4b63596be9dd74
민수 <109027302+Pushedsu@users.noreply.github.com>
2023-11-11 16:46:12 +0900

    검색 기능 추가

--- lib/pages/Bulletin/bulletin_board_page.dart
+++ lib/pages/Bulletin/bulletin_board_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:http/src/response.dart';
 import 'package:pet_care/connect/connect_server.dart';
 import 'package:pet_care/providers/current_post_provider.dart';
 import 'package:pet_care/widgets/text_field.dart';
@@ -19,18 +20,28 @@ class BulletinBoardPage extends StatefulWidget {
 
 class _BulletinBoardPageState extends State<BulletinBoardPage> {
   TextEditingController searchController = TextEditingController();
+
   List postsAuthorNameData = [];
   List postsObjectIdData = [];
   List postsContentsData = [];
   List postsTitleData = [];
   List postsLikeCount = [];
+
   static int count = 0;
+
+  String sendText = '';
+
+  final choiceSearchList = ['제목', '본문'];
+  var selectedSearchList = '제목';
 
   @override
   void initState() {
     super.initState();
     postsAuthorNameData = new List.empty(growable: true);
     postsContentsData = new List.empty(growable: true);
+    postsTitleData = new List.empty(growable: true);
+    postsLikeCount = new List.empty(growable: true);
+    postsObjectIdData = new List.empty(growable: true);
   }
 
   @override
@@ -38,28 +49,47 @@ class _BulletinBoardPageState extends State<BulletinBoardPage> {
     postsTitleData.clear();
     postsAuthorNameData.clear();
     postsContentsData.clear();
+    postsLikeCount.clear();
+    postsObjectIdData.clear();
     // TODO: implement dispose
     super.dispose();
   }
 
+  void clearData() {
+    postsTitleData.clear();
+    postsAuthorNameData.clear();
+    postsContentsData.clear();
+    postsLikeCount.clear();
+    postsObjectIdData.clear();
+    count = 0;
+  }
+
   Future getPosts() async {
-    var response = await Get().getAllPosts();
-    if(response.statusCode == 200 ) {
-      GetAllPostsModel postRes = GetAllPostsModel.fromJson(jsonDecode(response.body));
-      setState(() {
-        for (int i = 0; i < postRes.data!.length.toInt(); i++) {
-          postsContentsData.add(postRes.data![i].contents);
-          postsAuthorNameData.add(postRes.data![i].name);
-          postsTitleData.add(postRes.data![i].title);
-          postsObjectIdData.add(postRes.data![i].id);
-          postsLikeCount.add(postRes.data![i].likeCount);
-        }
-        count = postRes.data!.length;
-      });
+    Response? response;
+    if(sendText == ''){
+      response = await Get().getAllPosts();
+    }
+    if(selectedSearchList == '본문' && sendText != '') {
+      var searchPosting = SearchModel(sendText).toJson();
+      response = await Post().searchContents(searchPosting);
+    }
+    if(selectedSearchList == '제목' && sendText != '') {
+      var searchPosting = SearchModel(sendText).toJson();
+      response = await Post().searchTitle(searchPosting);
+    }
+    if (response?.statusCode == 200 || response?.statusCode == 201) {
+      GetAllPostsModel postRes = GetAllPostsModel.fromJson(jsonDecode(response!.body));
+      for (int i = 0; i < postRes.data!.length.toInt(); i++) {
+        postsContentsData.add(postRes.data![i].contents);
+        postsAuthorNameData.add(postRes.data![i].name);
+        postsTitleData.add(postRes.data![i].title);
+        postsObjectIdData.add(postRes.data![i].id);
+        postsLikeCount.add(postRes.data![i].likeCount);
+      }
+      count = postRes.data!.length;
       return 'Success';
     } else {
-      ResIsBoolFail res = ResIsBoolFail.fromJson(jsonDecode(response.body));
-      print('${res.message}');
+      ResIsBoolFail res = ResIsBoolFail.fromJson(jsonDecode(response!.body));
       return 'Fail';
     }
   }
@@ -67,7 +97,7 @@ class _BulletinBoardPageState extends State<BulletinBoardPage> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ()=> FocusScope.of(context).unfocus(),
+      onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         body: Stack(
           children: [
@@ -86,7 +116,9 @@ class _BulletinBoardPageState extends State<BulletinBoardPage> {
                 physics: NeverScrollableScrollPhysics(),
                 child: Column(
                   children: [
-                    SizedBox(height: 10.h,),
+                    SizedBox(
+                      height: 10.h,
+                    ),
                     Container(
                       decoration: BoxDecoration(
                           boxShadow: [
@@ -95,18 +127,19 @@ class _BulletinBoardPageState extends State<BulletinBoardPage> {
                               spreadRadius: 0,
                               blurRadius: 5.0,
                               offset:
-                              Offset(0, 10), // changes position of shadow
+                                  Offset(0, 10), // changes position of shadow
                             ),
                           ],
                           color: Colors.white,
-                          borderRadius: BorderRadius.all(Radius.circular(10))
-                      ),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
                       alignment: Alignment.center,
-                      padding: EdgeInsets.only(top: 15,bottom: 15),
-                      margin: EdgeInsets.only(left: 20.w,right: 20.w),
+                      padding: EdgeInsets.only(top: 15, bottom: 15),
+                      margin: EdgeInsets.only(left: 20.w, right: 20.w),
                       child: Row(
                         children: [
-                          SizedBox(width: 10.w,),
+                          SizedBox(
+                            width: 10.w,
+                          ),
                           Text(
                             '게시판',
                             textAlign: TextAlign.left,
@@ -116,25 +149,78 @@ class _BulletinBoardPageState extends State<BulletinBoardPage> {
                                 fontSize: 35.sp),
                           ),
                           SizedBox(
-                            width: 40.w,
-                          ),
-                          Icon(
-                            Icons.search_rounded,
-                            color: Colors.teal,
-                            size: 35.sp,
+                            width: 20.w,
                           ),
                           Container(
-                            width: 190.w,
+                            child: DropdownButton(
+                              value: selectedSearchList,
+                              items: choiceSearchList
+                                  .map((value) => DropdownMenuItem(
+                                      value: value, child: Text(value)))
+                                  .toList(),
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedSearchList = value!;
+                                });
+                              },
+                              style: TextStyle(
+                                  color: Colors.teal,
+                                  fontSize: 25.sp,
+                                  fontWeight: FontWeight.bold),
+                              underline: SizedBox(),
+                            ),
+                          ),
+                          Container(
+                            width: 160.w,
                             height: 40.h,
                             child: CustomTextField(
                                 text: 'Search',
                                 hintText: '검색어를 입력하시오.',
-                                controller: searchController),
+                                controller: searchController,
+                            ),
                           ),
+                          SizedBox(
+                            width: 10.w,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              if(searchController.text != '') {
+                                sendText = searchController.text;
+                                clearData();
+                                setState(() {
+                                  getPosts();
+                                });
+                              } else if(searchController.text == '') {
+                                sendText = searchController.text;
+                                clearData();
+                                setState(() {
+                                  getPosts();
+                                });
+                              } else {
+                                showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) => AlertDialog(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                      ),
+                                      title: Text('다시 입력하시오'),
+                                      actions: <Widget>[
+                                        TextButton(
+                                            onPressed: () => Navigator.pop(context),
+                                            child: Text('Ok')),
+                                      ],
+                                    )
+                                );
+                              }
+                            },
+                            child: Icon(Icons.send,color: Colors.teal,)
+                          )
                         ],
                       ),
                     ),
-                    SizedBox(height: 20.h,),
+                    SizedBox(
+                      height: 20.h,
+                    ),
                     FutureBuilder(
                         future: getPosts(),
                         builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -155,21 +241,27 @@ class _BulletinBoardPageState extends State<BulletinBoardPage> {
                                     )
                                   : ListView.builder(
                                       padding: EdgeInsets.only(top: 30.h),
-                                      itemBuilder:
-                                          (BuildContext context, int index) {
+                                      itemBuilder: (BuildContext context, int index) {
                                         return Column(
                                           children: [
                                             GestureDetector(
                                               onTap: () {
-                                                Provider.of<CurrentPostProvider>(context,listen: false).setObjectId(postsObjectIdData![index]);
-                                                Provider.of<CurrentPostProvider>(context,listen: false).setTitle(postsTitleData![index]);
-                                                Provider.of<CurrentPostProvider>(context,listen: false).setContents(postsContentsData![index]);
-                                                Provider.of<CurrentPostProvider>(context,listen: false).setName(postsAuthorNameData![index]);
-                                                Provider.of<CurrentPostProvider>(context,listen: false).setLikeCount(postsLikeCount![index]);
-                                                Navigator.pushNamed(context,'/post');
+                                                Provider.of<CurrentPostProvider>(context, listen: false)
+                                                    .setObjectId(postsObjectIdData![index]);
+                                                Provider.of<CurrentPostProvider>(context, listen: false)
+                                                    .setTitle(postsTitleData![index]);
+                                                Provider.of<CurrentPostProvider>(context, listen: false)
+                                                    .setContents(postsContentsData![index]);
+                                                Provider.of<CurrentPostProvider>(context, listen: false)
+                                                    .setName(postsAuthorNameData![index]);
+                                                Provider.of<CurrentPostProvider>(context, listen: false)
+                                                    .setLikeCount(postsLikeCount![index]);
+                                                Navigator.pushNamed(
+                                                    context, '/post');
                                               },
                                               child: Container(
-                                                padding: EdgeInsets.only(top: 10.h, bottom: 10.h),
+                                                padding: EdgeInsets.only(
+                                                    top: 10.h, bottom: 10.h),
                                                 width: 360.w,
                                                 decoration: BoxDecoration(
                                                     boxShadow: [
@@ -178,15 +270,22 @@ class _BulletinBoardPageState extends State<BulletinBoardPage> {
                                                             .withOpacity(0.7),
                                                         spreadRadius: 0,
                                                         blurRadius: 5.0,
-                                                        offset: Offset(0, 10), // changes position of shadow
+                                                        offset: Offset(0,
+                                                            10), // changes position of shadow
                                                       ),
                                                     ],
                                                     color: Colors.white,
-                                                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                                                    borderRadius:
+                                                        BorderRadius.all(
+                                                            Radius.circular(
+                                                                10))),
                                                 child: Container(
-                                                  margin: EdgeInsets.only(left: 15.w, right: 15.w),
+                                                  margin: EdgeInsets.only(
+                                                      left: 15.w, right: 15.w),
                                                   child: Column(
-                                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
                                                     children: [
                                                       Container(
                                                         width: 300.w,
@@ -194,16 +293,18 @@ class _BulletinBoardPageState extends State<BulletinBoardPage> {
                                                           '${postsTitleData![index]}',
                                                           style: TextStyle(
                                                               fontSize: 35.sp,
-                                                              color: Colors.teal),
-                                                          overflow:
-                                                              TextOverflow.ellipsis,
+                                                              color: Colors.teal
+                                                          ),
+                                                          overflow: TextOverflow.ellipsis,
                                                         ),
                                                       ),
                                                       Divider(
                                                         color: Colors.teal,
                                                         thickness: 1,
                                                       ),
-                                                      SizedBox(height: 5.h,),
+                                                      SizedBox(
+                                                        height: 5.h,
+                                                      ),
                                                       Text(
                                                         '${postsAuthorNameData![index]}',
                                                         style: TextStyle(
@@ -211,7 +312,9 @@ class _BulletinBoardPageState extends State<BulletinBoardPage> {
                                                           color: Colors.teal,
                                                         ),
                                                       ),
-                                                      SizedBox(height: 5.h,),
+                                                      SizedBox(
+                                                        height: 5.h,
+                                                      ),
                                                       Container(
                                                         child: Row(
                                                           children: [
@@ -222,19 +325,24 @@ class _BulletinBoardPageState extends State<BulletinBoardPage> {
                                                                 style: TextStyle(
                                                                     fontSize: 20.sp,
                                                                     color: Colors.teal),
-                                                                overflow: TextOverflow.ellipsis,
+                                                                overflow:
+                                                                    TextOverflow.ellipsis,
                                                               ),
                                                             ),
                                                           ],
                                                         ),
                                                       ),
-                                                      SizedBox(height: 5.h,),
+                                                      SizedBox(
+                                                        height: 5.h,
+                                                      ),
                                                     ],
                                                   ),
                                                 ),
                                               ),
                                             ),
-                                            SizedBox(height: 30.h,),
+                                            SizedBox(
+                                              height: 30.h,
+                                            ),
                                           ],
                                         );
                                       },
